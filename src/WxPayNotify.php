8b1eebf4e06dfeee232afcc9e12d6140252793ed
iflamed <iflamed@gmail.com>
2015-09-12 00:50:32 +0800

    typo style

--- src/WxPayNotify.php
+++ src/WxPayNotify.php
@@ -14,22 +14,22 @@ class WxPayNotify extends WxPayNotifyReply
      * 回调入口
      * @param bool $needSign  是否需要签名输出
      */
-    final public function Handle($needSign = true)
+    final public function handle($needSign = true)
     {
         $msg = "OK";
-        //当返回false的时候，表示notify中调用NotifyCallBack回调失败获取签名校验失败，此时直接回复失败
-        $result = WxpayApi::notify(array($this, 'NotifyCallBack'), $msg);
+        //当返回false的时候，表示notify中调用notifyCallBack回调失败获取签名校验失败，此时直接回复失败
+        $result = WxpayApi::notify(array($this, 'notifyCallBack'), $msg);
         if ($result == false) {
             $this->SetReturn_code("FAIL");
             $this->SetReturn_msg($msg);
-            $this->ReplyNotify(false);
+            $this->replyNotify(false);
             return;
         } else {
-            //该分支在成功回调到NotifyCallBack方法，处理完成之后流程
+            //该分支在成功回调到notifyCallBack方法，处理完成之后流程
             $this->SetReturn_code("SUCCESS");
             $this->SetReturn_msg("OK");
         }
-        $this->ReplyNotify($needSign);
+        $this->replyNotify($needSign);
     }
 
     /**
@@ -42,7 +42,7 @@ class WxPayNotify extends WxPayNotifyReply
      * @param string $msg 如果回调处理失败，可以将错误信息输出到该方法
      * @return true回调出来完成不需要继续回调，false回调处理未完成需要继续回调
      */
-    public function NotifyProcess($data, &$msg)
+    public function notifyProcess($data, &$msg)
     {
         //TODO 用户基础该类之后需要重写该方法，成功的时候返回true，失败返回false
         return true;
@@ -54,10 +54,10 @@ class WxPayNotify extends WxPayNotifyReply
      * @param array $data
      * @return true回调出来完成不需要继续回调，false回调处理未完成需要继续回调
      */
-    final public function NotifyCallBack($data)
+    final public function notifyCallBack($data)
     {
         $msg = "OK";
-        $result = $this->NotifyProcess($data, $msg);
+        $result = $this->notifyProcess($data, $msg);
 
         if ($result == true) {
             $this->SetReturn_code("SUCCESS");
@@ -74,7 +74,7 @@ class WxPayNotify extends WxPayNotifyReply
      * 回复通知
      * @param bool $needSign 是否需要签名输出
      */
-    final private function ReplyNotify($needSign = true)
+    final private function replyNotify($needSign = true)
     {
         //如果需要签名
         if ($needSign == true &&
